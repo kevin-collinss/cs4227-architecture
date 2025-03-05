98f1ecbeefb184a7cac6a45d7bca9cbb848d5056
Kevin Collins <kevinmscollins@gmail.com>
2025-03-05 12:50:01 +0000

    transfer

---
+++ frontend/tms/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RegistrationForm from './components/Registration/RegistrationForm';
+import Home from './components/Home'; // Your existing home component
+import RestaurantList from './components/Restaurant/RestaurantList';
+import RestaurantDetail from './components/Restaurant/RestaurantDetail';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<RegistrationForm />} />
+                {/*Routes for restaurant discovery */}
+                <Route path="/restaurants" element={<RestaurantList />} />
+                <Route path="/restaurants/:id" element={<RestaurantDetail />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
